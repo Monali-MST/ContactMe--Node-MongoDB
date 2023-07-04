91d3b7e16792f294a04aef95eb4045cb7eafae08
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-07-05 02:00:30 +0530

    user register with bcrypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,25 +1,46 @@
 const asyncHandler = require("express-async-handler");
-const Contact = require("../models/contactModel");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 //@desc register a user
 //@route POST/api/user/register
 //@access public
-const registerUser = asyncHandler(async (req,res)=>{
-    res.json({message: "created user"});
+const registerUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory !");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already registered!");
+  }
+
+  //Hash Password0112214565
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const user = await User.create({ username, email, password: hashedPassword });
+
+  if (user) {
+    res.status(201).json({ _id: user.id, email: user.email });
+  }else{
+    res.status(400);
+    throw new Error("User data is not valid");
+  }
 });
 
 //@desc login a user
 //@route POST/api/user/login
 //@access public
-const loginUser = asyncHandler(async (req,res)=>{
-    res.json({message: "Login user"});
+const loginUser = asyncHandler(async (req, res) => {
+  res.json({ message: "Login user" });
 });
 
 //@desc current user infor
 //@route GET/api/user/current
 //@access private
-const currentUser = asyncHandler(async (req,res)=>{
-    res.json({message: "current user infor"});
+const currentUser = asyncHandler(async (req, res) => {
+  res.json({ message: "current user infor" });
 });
 
-module.exports = {registerUser,loginUser ,currentUser};
+module.exports = { registerUser, loginUser, currentUser };
--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ const dotenv = require("dotenv").config();
 connectDb();
 const app = express();
 
-const port= process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/user", require("./routes/userRoutes"));
 app.use(errorHandler);
 
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
