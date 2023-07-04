ef97f4e59ad6fce2e495203201b5a1f3c7ef9a54
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-07-04 17:59:23 +0530

    connect mongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-const errorHandler = require("./middleware/errorhandler");
-const app = express();
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+
+connectDb();
+const app = express();
+
 const port= process.env.PORT || 5000;
 
 app.use(express.json())
