d8c7302b7e5ca6f29d73f2b4960b9f3f2549bca7
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-07-05 23:21:53 +0530

    install jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
 //@desc register a user
