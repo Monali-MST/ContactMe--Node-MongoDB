f8441966f198f7d7592c5c687de3ff510a0cb4ba
Monali Shiwanthika <shiwanthikamonali@gmail.com>
2023-08-03 11:39:49 +0530

    login time change. testing done. phone no not uniq

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -51,7 +51,7 @@ const loginUser = asyncHandler(async (req, res) => {
         },
       },
       process.env.ACCESS_TOKEN_SECERT,
-      { expiresIn: "30m" }
+      { expiresIn: "24h" }
     );
     res.status(200).json({ accessToken });
   } else {
